d5cde47dcd207c700ed3d5882ae01a084abf356b
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-09-21 14:24:58 +0200

    init fix

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "reality-codable",
+    platforms: [
+      .iOS(.v15),
+      .macOS(.v12),
+      .visionOS(.v1),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RealityCodable",
+            targets: ["RealityCodable"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RealityCodable"),
+        .testTarget(
+            name: "RealityCodableTests",
+            dependencies: ["RealityCodable"]),
+    ]
+)
